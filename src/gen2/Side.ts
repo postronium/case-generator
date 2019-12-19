bfc826a5dd975ef0fe4777ea1e459a4aa23b98f7
Adrian Buntschu <adrian.buntschu@edu.hefr.ch>
2019-12-19 15:00:02 +0100

    Delete useless files

--- src/gen2/Side.ts
+++
@@ -1,5 +0,0 @@
-export enum Side {
-    SIDE = 0,
-    TOP = 1,
-    BOTTOM = 2
-}
--- src/js/constants/action-types.ts
+++
@@ -1,7 +0,0 @@
-export const ADD_PCB = "ADD_PCB";
-export const REMOVE_PCB = "REMOVE_PCB";
-export const TOGGLE_HOLE = "TOGGLE_HOLE";
-export const TOGGLE_DISPLAY_TOP = "TOGGLE_DISPLAY_TOP";
-export const MOVE_PCB = "MOVE_PCB";
-export const SET_SHIELD = "SET_SHIELD";
-export const REMOVE_SHIELD = "REMOVE_SHIELD";
